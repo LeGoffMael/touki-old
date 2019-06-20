9f1d9608114760bf2af1e689f874baac1a97edfa
hcylia <36785991+hcylia@users.noreply.github.com>
2019-06-20 17:42:34 +0200

    Conflict on liquibase data

--- src/main/webapp/app/modules/home/home.tsx
+++ src/main/webapp/app/modules/home/home.tsx
@@ -20,35 +20,63 @@ export class Home extends React.Component<IHomeProp> {
     const { account } = this.props;
     return (
       <Row>
-        {/* Side bar */}
-        <Col md="3">
-          <div className="profilNav">
-            <div className="userInfoNav">
-              <div className="profilPhotoNav">
-                {' '}
-                <img />
-              </div>
-              <p className="text-center font-weight-bold"> {account.login}</p>
-              <p className="text-left">{account && account.badge ? account.badge : '-no badge-'}</p>
-            </div>
-            <div className="text-left tripInforNav">
-              <a href="/entity/travel">New trip</a> <br />
-              <a href="">My trips</a>
-            </div>
-          </div>
-        </Col>
-        {/* Wall */}
-        <Col md="6">
-          <div className="wall">
-            <p className="text-left font-weight-bold"> News</p>
-          </div>
-        </Col>
         {/* Trending destinations */}
         <Col md="3">
           <div className="trendingDestination">
             <p className="text-center font-weight-bold"> Trending destinations</p>
             <div className="trendingDestinationList" />
           </div>
+        </Col>
+        {/* Wall */}
+        <Col md="9">
+          <h1>TIMELINE</h1>
+          {/* section 1 */}
+          <section id="timeline" className="timeline-container">
+            <div className="timeline-block">
+              <div className="timeline-icon picture">
+                <img src="http://cristykoebler.com/picture_library/picture.svg" alt="Picture" />
+              </div>
+              <div className="timeline-content">
+                <h2>Born</h2>
+                <img src="http://cristykoebler.com/picture_library/Timeline/Baby150.gif" alt="I was born in 1983" />
+                <span className="timeline-date">
+                  <h3>1983</h3>
+                </span>
+              </div>
+            </div>
+            {/* section 2 */}
+            <div className="timeline-block">
+              <div className="timeline-icon picture">
+                <img src="http://cristykoebler.com/picture_library/picture.svg" alt="Picture" />
+              </div>
+              <div className="timeline-content">
+                <h2>Hit in the head with a golf club by my brother (but I still think I’m a tough girl)</h2>
+                <img
+                  src="http://cristykoebler.com/picture_library/Timeline/HitByGolfClub150.gif"
+                  alt="Hit in the head with a golf club by my brother (but I'm still pretty tough)"
+                />
+                <span className="timeline-date">
+                  <h3>1986</h3>
+                </span>
+              </div>
+            </div>
+            {/* section 3 */}
+            <div className="timeline-block">
+              <div className="timeline-icon picture">
+                <img src="http://cristykoebler.com/picture_library/picture.svg" alt="Picture" />
+              </div>
+              <div className="timeline-content">
+                <h2>Discovering that weird runs in the family</h2>
+                <img
+                  src="http://cristykoebler.com/picture_library/Timeline/WeirdFamily150.gif"
+                  alt="Discovering that weird runs in the family"
+                />
+                <span className="timeline-date">
+                  <h3>1989</h3>
+                </span>
+              </div>
+            </div>
+          </section>
         </Col>
       </Row>
     );
